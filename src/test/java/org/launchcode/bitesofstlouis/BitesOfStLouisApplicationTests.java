6d6f6cb33572090d8e01c1f288582ceca3374152
iainxiao <iainxiao@gmail.com>
2020-08-15 22:17:42 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/bitesofstlouis/BitesOfStLouisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.bitesofstlouis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitesOfStLouisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
